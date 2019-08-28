930f45a635b1627441ef3a3953dcbba9de12c4b2
Nino <nino.petrovic@manto.net>
2019-08-29 01:13:16 +0200

    v1.0.3 minor UI upgrades

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,7 +9,7 @@ declare const ej: any;
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit, AfterViewInit {
     @ViewChild('thesTreeView') thesTreeView: TreeViewComponent;
@@ -17,40 +17,11 @@ export class TestComponent implements OnInit, AfterViewInit {
     fields: Object;
     constructor(private testService: TestService) { }
 
-    public hierarchicalData: Object[] = [
-        { id: '01', name: 'Music', expanded: true,
-            subChild: [
-                {id: '01-01', name: 'Gouttes.mp3'},
-            ]
-        },
-        {
-            id: '02', name: 'Videos',
-            subChild: [
-                {id: '02-01', name: 'Naturals.mp4'},
-                {id: '02-02', name: 'Wild.mpeg'}
-            ]
-        },
-        {
-            id: '03', name: 'Documents',
-            subChild: [
-                {id: '03-01', name: 'Environment Pollution.docx'},
-                {id: '03-02', name: 'Global Water, Sanitation, & Hygiene.docx'},
-                {id: '03-03', name: 'Global Warming.ppt'},
-                {id: '03-02', name: 'Social Network.pdf'},
-                {id: '03-03', name: 'Youth Empowerment.pdf'},
-            ]
-        }
-    ];
-    public field = { dataSource: this.hierarchicalData, id: 'name', text: 'name', child: 'subChild' };
-
     ngOnInit() {
         $.getJSON('https://raw.githubusercontent.com/d3/d3-hierarchy/v1.1.8/test/data/flare.json', (res) => {
             console.log(res);
             this.dataSource = res;
-            const dummyData = {'name': 'bla bla'};
-            this.field.dataSource = [this.dataSource];
-            this.field.child = 'children';
-            this.fields = this.field;
+            this.fields = { dataSource: this.dataSource['children'], id: 'name', text: 'name', child: 'children' };
         }).done(() => {
             console.log('data', this.dataSource);
         });
